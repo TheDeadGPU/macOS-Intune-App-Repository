f4f2ac65383fa515ecb54e26b3e549306703e45d
TheDeadGPU <girardj@go.stockton.edu>
2021-04-13 13:17:09 -0400

    Updated Firefox Packager to Pull Official PKG
    Fun fact! Spaces in the package name breaks the Intune app install process!

--- Firefox/FirefoxPackager.sh
+++ Firefox/FirefoxPackager.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #Author: TheDeadGPU
 
-#Check to see if the macOS Intune Packager is Installed
+#Check to see if the macOS Intune Packager is installed
 if test -e "/Applications/IntuneAppUtil"; then
-    curl -LOk "https://download-installer.cdn.mozilla.net/pub/firefox/releases/87.0/mac/en-US/Firefox%2087.0.dmg"
-    mv "Firefox%2087.0.dmg" "Firefox 87.0.dmg"
-    hdiutil attach "Firefox 87.0.dmg"
-    sudo pkgbuild --install-location /Applications --component /Volumes/Firefox/Firefox.app ./Firefox.pkg
-    hdiutil detach "/Volumes/Firefox"
-    /Applications/IntuneAppUtil -c Firefox.pkg -o .
-    rm -rf "Firefox 87.0.dmg"
-    rm -rf "Firefox.pkg"
+    curl -LOk "https://download-installer.cdn.mozilla.net/pub/firefox/releases/87.0/mac/en-US/Firefox%2087.0.pkg"
+    mv "Firefox%2087.0.pkg" "Firefox87.0.pkg"
+    /Applications/IntuneAppUtil -c Firefox87.0.pkg -o .
+    rm -rf "Firefox87.0.pkg"
 else
     echo "Intune Packager not installed. Please install the Intune Packager and then run the script again."
 fi
